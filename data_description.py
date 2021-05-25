1fe2f56e7822ead79037578d64ae61ee36651ebb
Priya Charaya <priyacharaya4163@gmail.com>
2021-05-26 01:28:57 +0530

    data-description

---
+++ data_description.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import sys
+
+class Description:
+    def __init__(self, dataset):
+        self.data=dataset
+        
+
+    def tasks(self):
+        while True:
+            print("Tasks (Data Description)")
+            print()
+            print("1. Describe a specific column")
+            print("2. Show properties of each column")
+            print("3. Show the dataset")
+            print()
+            val=int(input("What do you want to do? (Press -1 to go back) "))
+
+            if val==1:
+                self.description_column()
+        
+            elif val==2:
+                self.properties()
+
+            elif val==3:
+                self.showData()
+
+            elif val==-1:
+                sys.exit()
+
+    def description_column(self):
+        print("Columns: ")
+        self.data=pd.read_csv("train.csv")
+        for i in self.data.columns:
+            print(i, end="  ")
+        print() 
+        while(True):
+            value=input("Enter the column for which you want a description :")
+            if value in self.data.columns:
+                print(self.data[value].describe())
+                break
+            else:
+                print("You entered the wrong target variable, which is not present in columns. So please re-enter the target variable")
+                continue
+
+    def properties(self):
+        print(self.data.describe())
+        print(self.data.info)
+
+    
+    def showData(self):
+        rows = int(input("Enter number of rows: "))
+        print(self.data.head(rows))
---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+import pandas as pd
+from data_description import Description
+
+class preProcessing:
+    def __init__(self):
+        if(len(sys.argv)!=2 or not(sys.argv[1].endswith(".csv"))):
+            print("Number of arguments doesn't meet the given condition.")
+            return 
+        else:
+            print("Welcome to the ML Preprocessing CLI")
+        
+    def remove_columns(self):
+        self.df=pd.read_csv("train.csv")
+        #print(self.df.columns)
+        for i in self.df.columns:
+            print(i, end="  ")
+        print() 
+        while(True): 
+            val=input("Enter the target variable to be removed: ")
+            #print(val)
+            val1=input("Are you sure?(y/n)")
+            if (val1=='y' or val1=='Y'):
+                if(val in self.df.columns):
+                    self.df.drop(val, axis=1, inplace=True)
+                    #print(self.df.columns)
+                    print("Done...")
+                    break
+                else:
+                    print("You entered the wrong target variable, which is not present in columns. So please re-enter the target variable")
+                    continue
+            elif val1!='y' and val1!='n' and val1!='Y' and val1!='N':
+                print("Since you have inputted the wrong value, you are exited from the program.")
+                sys.exit()
+
+    def tasks(self):
+        print("Tasks (Preprocessing)")
+        print()
+        print("1. Data Description" )
+        print("2. Handling NULL Values")
+        print("3. Encoding Categorical Data")
+        print("4. Feature Scaling of the Dataset")
+        print("5. Dowload the modified dataset")
+
+        val=int(input("What do you want to do ? (Press -1 to exit) "))
+        
+        if(val==1):
+            obj1=Description(self.df)
+            obj1.tasks()
+
+            
+            
+
+
+
+
+obj = preProcessing()
+obj.remove_columns()
+obj.tasks()
